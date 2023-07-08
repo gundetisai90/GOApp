ff5396c760a89ec9e7e5cc73924de3185bfb9b64
gundetisai90 <saireddygundeti902@gmail.com>
2023-07-08 18:16:12 +0530

    final commit

--- src/components/Card.js
+++ src/components/Card.js
@@ -15,32 +15,35 @@ export default function Card(props) {
     let foodItem = props.fooditems;
 
     const handleAddToCart = async () => {
-        // console.log(foodItem);
-        let food = [];
+        let food = []
         for (const item of data) {
-            if (item.id === foodItem._id) {
-                food = data;
-                break;
-            }
+          if (item.id === foodItem._id) {
+            food = item;
+    
+            break;
+          }
         }
-        console.log(food);
+        
         if (food !== []) {
-            if (food.size === size) {
-                await dispatch({ type: "UPDATE", id: foodItem._id, price: finalPrice, qty: qty })
-                return
-            }
-            else if (food.size !== size) {
-                await dispatch({ type: "ADD", id: foodItem._id, name: foodItem.name, price: finalPrice, qty: qty, size: size, img: foodItem.img })
-
-                return
-            }
+          if (food.size === size) {
+            await dispatch({ type: "UPDATE", id: foodItem._id, price: finalPrice, qty: qty })
             return
+          }
+          else if (food.size !== size) {
+            await dispatch({ type: "ADD", id: foodItem._id, name: foodItem.name, price: finalPrice, qty: qty, size: size,img: foodItem.img })
+            console.log("Size different so simply ADD one more to the list")
+            return
+          }
+          return
         }
-
+    
         await dispatch({ type: "ADD", id: foodItem._id, name: foodItem.name, price: finalPrice, qty: qty, size: size })
-
+    
+    
         // setBtnEnable(true)
-    }
+    
+      }
+    
     let finalPrice = qty * parseInt(options[size]);
     useEffect(() => {
         setsize(priceRef.current.value)
